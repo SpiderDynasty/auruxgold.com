406d37dde6a8c028b14b5310b133b7ab04692b56
SpiderDynasty <business.spiderdynasty@gmail.com>
2026-04-05 10:13:11 +0200

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AuctionSite from "./app";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AuctionSite />
+  </React.StrictMode>
+);
